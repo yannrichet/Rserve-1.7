aeef5e98e51b9d891a2ea9f9a7c28e11840162ef
Yann Richet <richetyann@gmail.com>
2018-08-13 17:35:28 +0200

    ...

--- tests/test-zzzend.R
+++ tests/test-zzzend.R
@@ -2,5 +2,13 @@ library("RSclient")
 
 c = RS.connect()
 
+
 RS.server.shutdown(c)
 RS.close(c)
+
+# hard end... (to be sure)
+if (.Platform$OS == "windows") {
+  system("taskkill /F /IM Rserve.exe")
+} else {
+  system("killall Rserve")
+}
