1272e22cbca6be2b831d235a740cce3251d4259f
Yann Richet <richetyann@gmail.com>
2018-08-18 11:02:03 +0200

    ...

--- tests/test-RserveBugJava.sh
+++ tests/test-RserveBugJava.sh
@@ -2,7 +2,8 @@
 
 javac -cp REngine-1.8.jar:RserveEngine-1.8.jar RserveBug.java
 
-Rscript -e "Rserve::Rserve()"
+Rscript -e "Rserve::Rserve(args='--vanilla --RS-enable-control')"
 
 java -cp .:REngine-1.8.jar:RserveEngine-1.8.jar RserveBug
 
+exit $?
