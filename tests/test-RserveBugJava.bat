22fb83b640c802e4a499e5e7feca6907e0a8953c
Yann Richet <richetyann@gmail.com>
2018-08-20 15:59:05 +0200

    Update test-RserveBugJava.bat

--- tests/test-RserveBugJava.bat
+++ tests/test-RserveBugJava.bat
@@ -1,6 +1,6 @@
 javac -cp REngine-1.8.jar;RserveEngine-1.8.jar RserveBug.java
 
-Rscript -e "install.packages('../../Rserve_1.7-4.zip'); Rserve::Rserve(args='--vanilla --RS-enable-control')"
+Rscript -e "install.packages('..'); Rserve::Rserve(args='--vanilla --RS-enable-control')"
 
 java -cp .;REngine-1.8.jar;RserveEngine-1.8.jar RserveBug
 
