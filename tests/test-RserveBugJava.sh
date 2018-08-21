14c14e73a5d03616124a62991bcef4bd4fbd379d
Yann Richet <richetyann@gmail.com>
2018-08-21 11:49:12 +0200

    ...

--- tests/test-RserveBugJava.sh
+++ tests/test-RserveBugJava.sh
@@ -4,7 +4,7 @@ javac -cp REngine-1.8.jar:RserveEngine-1.8.jar RserveBug.java
 
 killall Rserve
 
-Rscript -e "sessionInfo(); Rserve::Rserve(args='--vanilla --RS-enable-control')"
+Rscript -e "sessionInfo(); packageDescription('Rserve'); Rserve::Rserve(args='--vanilla --RS-enable-control')"
 
 java -cp .:REngine-1.8.jar:RserveEngine-1.8.jar RserveBug
 RET=$?
